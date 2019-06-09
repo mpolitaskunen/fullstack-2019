e561335803a6e199f797cb606c1c95cf24f2dd4e
mpolitaskunen <36896696+mpolitaskunen@users.noreply.github.com>
2019-06-09 12:33:17 +0300

    eslint cleanup

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -8,10 +8,10 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
@@ -24,24 +24,24 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+    const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
