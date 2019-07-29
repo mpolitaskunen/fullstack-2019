c7cbde803a0b4a6506e1bafd3b2df9345a10ecd0
mpolitaskunen <36896696+mpolitaskunen@users.noreply.github.com>
2019-07-29 16:33:59 +0300

    Part 6 - Initial configuration, added packages required for eslint, checked that code gets cleaned.

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -7,10 +7,10 @@ import reducer from './reducers/anecdoteReducer'
 const store = createStore(reducer)
 
 const render = () => {
-  ReactDOM.render(
-    <App store={store} />,
-    document.getElementById('root')
-  )
+    ReactDOM.render(
+        <App store={store} />,
+        document.getElementById('root')
+    )
 }
 
 render()
