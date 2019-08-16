955e7642c1601a42d94969ce05f64e534baf17cb
Seppo Taskunen <seppo.taskunen@taskupalvelut.local>
2019-08-16 20:13:27 +0300

    6.18 Refactored to use redux-thunk

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,16 +1,14 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import doteService from './services/anecdotes'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 
-const App = () => {
+const App = (props) => {
     useEffect(() => {
-        doteService
-            .getAll().then(anecdotes => initializeAnecdotes(anecdotes))
+        props.initializeAnecdotes()
     },[])
 
     return (
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,21 +1,4 @@
-// const anecdotesAtStart = [
-//    'If it hurts, do it more often',
-//    'Adding manpower to a late software project makes it later!',
-//    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-//    'Premature optimization is the root of all evil.',
-//    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-//]
-
-// const getId = () => (100000 * Math.random()).toFixed(0)
-
-//const asObject = (anecdote) => {
-//    return {
-//        content: anecdote,
-//        id: getId(),
-//        votes: 0
-//    }
-//}
+import doteService from '../services/anecdotes'
 
 export const addDote = (anecdote) => {
     return {
@@ -35,18 +18,16 @@ export const addVote = (id) => {
 }
 
 export const initializeAnecdotes = (anecdotes) => {
-    return {
-        type: 'INITIALIZE_ANECDOTES',
-        data: anecdotes,
+    return async dispatch => {
+        const anecdotes = await doteService.getAll()
+        dispatch({
+            type: 'INITIALIZE_ANECDOTES',
+            data: anecdotes,
+        })
     }
 }
 
-// const initialState = anecdotesAtStart.map(asObject)
-
 const reducer = (state = [], action) => {
-    // Let's disable this console message...
-    // console.log('anecdoteReducer: state, action: ', state, action)
-
     switch(action.type) {
 
     case 'NEW': {
